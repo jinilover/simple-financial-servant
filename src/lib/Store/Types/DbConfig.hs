1a15375b4188070295e5c173436bbe41901fbe48
jinilover <columbawong@gmail.com>
2026-01-13 18:03:08 +1100

    Remove unnecessary DeriveAnyClass, DerivingVia from newtype deriving Show

--- src/lib/Store/Types/DbConfig.hs
+++ src/lib/Store/Types/DbConfig.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE DeriveAnyClass #-}
-{-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE TemplateHaskell #-}
 module Store.Types.DbConfig where
 
@@ -11,17 +9,17 @@ import Common.Types
 
 newtype DbConnPoolSize = DbConnPoolSize
   { unDbConnPoolSize :: PosInt }
-  deriving Show via PosInt
+  deriving Show
 makeClassy ''DbConnPoolSize
 
 newtype DbConnString = DbConnString
   { unDbConnString :: ByteString }
-  deriving Show via ByteString
+  deriving Show
 makeClassy ''DbConnString
 
 newtype DbSchema = DbSchema
   { unDbSchema :: Text }
-  deriving Show via Text
+  deriving Show
 makeClassy ''DbSchema
 
 data DbConfig = DbConfig 
