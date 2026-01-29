2bbc4d45b3ff38ddb1a8182e211bee8d47a03bd8
jinilover <columbawong@gmail.com>
2026-01-30 00:45:42 +1100

    Replace String by Text

--- src/lib/Plaid/Types/InstitutionId.hs
+++ src/lib/Plaid/Types/InstitutionId.hs
@@ -1,8 +1,9 @@
 module Plaid.Types.InstitutionId where
 
 import Data.Aeson
+import Data.Text
 
 data InstitutionId = Institution3
 
 instance ToJSON InstitutionId where
-  toJSON Institution3 = toJSON ("ins_3" :: String)
+  toJSON Institution3 = toJSON ("ins_3" :: Text)
--- src/lib/Plaid/Types/Product.hs
+++ src/lib/Plaid/Types/Product.hs
@@ -1,10 +1,10 @@
 module Plaid.Types.Product where
 
 import Data.Aeson
-
+import Data.Text
 
 data Product = Auth | Transactions
 
 instance ToJSON Product where
-  toJSON Auth = toJSON ("auth" :: String)
-  toJSON Transactions = toJSON ("transactions" :: String)
+  toJSON Auth = toJSON ("auth" :: Text)
+  toJSON Transactions = toJSON ("transactions" :: Text)
